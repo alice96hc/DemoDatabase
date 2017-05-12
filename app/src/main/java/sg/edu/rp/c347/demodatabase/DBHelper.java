7459fafbe8011b5f60ff3e744cd2aa3254783ff5
15017199 <15017199@myrp.edu.sg>
2017-05-12 13:51:49 +0800

    - 1st

--- app/src/main/java/sg/edu/rp/c347/demodatabase/DBHelper.java
+++ app/src/main/java/sg/edu/rp/c347/demodatabase/DBHelper.java
@@ -1,11 +1,13 @@
 package sg.edu.rp.c347.demodatabase;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import static android.R.attr.version;
+import java.util.ArrayList;
 
 
 /**
@@ -20,7 +22,7 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "tasks.db";
     private static final String TABLE_TASK = "task";
     private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_DESCRIPTION = "description"
+    private static final String COLUMN_DESCRIPTION = "description";
     private static final String COLUMN_DATE = "date";
 
     public DBHelper(Context context) {
@@ -38,8 +40,54 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int
-            newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+    public void insertTask(String description, String date){
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+        // Store the column name as key and the description as value
+        values.put(COLUMN_DESCRIPTION, description);
+        // Store the column name as key and the date as value
+        values.put(COLUMN_DATE, date);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_TASK, null, values);
+        // Close the database connection
+        db.close();
+    }
+    public ArrayList<String> getTaskContent() {
+        // Create an ArrayList that holds String objects
+        ArrayList<String> tasks = new ArrayList<String>();
+        // Select all the tasks' description
+        String selectQuery = "SELECT " + COLUMN_DESCRIPTION
+                + " FROM " + TABLE_TASK;
+
+        // Get the instance of database to read
+        SQLiteDatabase db = this.getReadableDatabase();
+        // Run the SQL query and get back the Cursor object
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // moveToFirst() moves to first row
+        if (cursor.moveToFirst()) {
+            // Loop while moveToNext() points to next row
+            //  and returns true; moveToNext() returns false
+            //  when no more next row to move to
+            do {
+                // Add the task content to the ArrayList object
+                //  0 in getString(0) return the data in the first
+                //  column in the Cursor object. getString(1)
+                //  return second column data and so on.
+                //  Use getInt(0) if data is an int
+                tasks.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return tasks;
     }
 }
--- app/src/main/java/sg/edu/rp/c347/demodatabase/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demodatabase/MainActivity.java
@@ -2,12 +2,29 @@ package sg.edu.rp.c347.demodatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+            }
+        });
     }
 }
