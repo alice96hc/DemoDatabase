e2d64cb52a9bd5db54ab704470a2cd29c649b0d8
15017199 <15017199@myrp.edu.sg>
2017-05-12 16:20:09 +0800

    - 3

--- app/src/main/java/sg/edu/rp/c347/demodatabase/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demodatabase/MainActivity.java
@@ -4,21 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import static sg.edu.rp.c347.demodatabase.R.id.lv;
+
 public class MainActivity extends AppCompatActivity {
     Button btnInsert,btnGetTasks;
     TextView tvResults;
+    ListView lv;
+    ArrayList<Task>task;
+    ArrayAdapter aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView)this.findViewById(R.id.lv);
+
+        task = new ArrayList<Task>();
+//        food.add(new Food("Ah Lat Coffee", false));
+//        food.add(new Food("Rocky Choc", true));
+//        food.add(new Food("Kid Cat Choc", true));
 
         btnInsert = (Button) findViewById(R.id.btnInsert);
         btnGetTasks = (Button) findViewById(R.id.btnGetTasks);
@@ -42,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
                 // Create the DBHelper object, passing in the
                 // activity's Context
                 DBHelper db = new DBHelper(MainActivity.this);
-
                 // Insert a task
                 ArrayList<String> data = db.getTaskContent();
                 db.close();
@@ -53,6 +66,13 @@ public class MainActivity extends AppCompatActivity {
                     txt += i + ". " + data.get(i) + "\n";
                 }
                 tvResults.setText(txt);
+
+                DBHelper db2 = new DBHelper(MainActivity.this);
+                ArrayList<Task> al = db2.getTasks();
+                db2.close();
+
+                TaskAdapter aa = new TaskAdapter(MainActivity.this,R.layout.row,al);
+                lv.setAdapter(aa);
             }
         });
     }
